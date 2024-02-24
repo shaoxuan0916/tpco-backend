4c36563d9ed913f466275054117acda8ce71be90
shaoxuan <shaoxuanhinhua@gmail.com>
2024-02-25 02:33:53 +0800

    first commit

---
+++ medusa-config.js
@@ -0,0 +1,120 @@
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: `@medusajs/file-local`,
+    options: {
+      upload_dir: "uploads",
+    },
+  },
+  // Admin Dashboard & Functionality
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      develop: {
+        open: process.env.OPEN_BROWSER !== "false",
+      },
+    },
+  },
+  // Storing products images
+  {
+    resolve: `medusa-storage-supabase`,
+    options: {
+      referenceID: process.env.STORAGE_BUCKET_REF,
+      serviceKey: process.env.STORAGE_SERVICE_KEY,
+      bucketName: process.env.BUCKET_NAME,
+    },
+  },
+  // Handle product variant images
+  {
+    resolve: "medusa-plugin-variant-images",
+    options: {
+      enableUI: true,
+    },
+  },
+  // AWS S3 Storage (backup solution)
+  // {
+  //   resolve: `medusa-file-s3`,
+  //   options: {
+  //     s3_url: process.env.S3_URL,
+  //     bucket: process.env.S3_BUCKET,
+  //     region: process.env.S3_REGION,
+  //     access_key_id: process.env.S3_ACCESS_KEY_ID,
+  //     secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+  //     cache_control: process.env.S3_CACHE_CONTROL,
+  //     // optional
+  //     download_file_duration: process.env.S3_DOWNLOAD_FILE_DURATION,
+  //     prefix: process.env.S3_PREFIX,
+  //   },
+  // },
+];
+
+const modules = {
+  /*eventBus: {
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },
+  cacheService: {
+    resolve: "@medusajs/cache-redis",
+    options: {
+      redisUrl: REDIS_URL
+    }
+  },*/
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
+const projectConfig = {
+  jwtSecret: process.env.JWT_SECRET,
+  cookieSecret: process.env.COOKIE_SECRET,
+  store_cors: STORE_CORS,
+  database_url: DATABASE_URL,
+  admin_cors: ADMIN_CORS,
+  // Uncomment the following lines to enable REDIS
+  // redis_url: REDIS_URL
+};
+
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig,
+  plugins,
+  modules,
+};
