0b2f709e6b71048819b1f17e98b714c25ea263d4
shaoxuan <shaoxuanhinhua@gmail.com>
2024-02-27 23:32:36 +0800

    Try fixxing deploy

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,8 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS ||
+  "http://localhost:7000,http://localhost:7001,https://tpco-admin-backend.vercel.app";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
@@ -48,6 +49,8 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
+      path: "/app",
+      backend: "https://tpco-backend-production.up.railway.app/",
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
