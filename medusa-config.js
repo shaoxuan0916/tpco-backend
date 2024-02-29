c286883041debe22ad69179015b55d8e549cc2ab
shaoxuan <shaoxuanhinhua@gmail.com>
2024-03-01 01:55:09 +0800

    Added algolia search -test

--- medusa-config.js
+++ medusa-config.js
@@ -68,6 +68,40 @@ const plugins = [
       enableUI: true,
     },
   },
+  // Algolia search
+  {
+    resolve: `medusa-plugin-algolia`,
+    options: {
+      applicationId: process.env.ALGOLIA_APP_ID,
+      adminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+      settings: {
+        settings: {
+          products: {
+            indexSettings: {
+              searchableAttributes: ["title", "description"],
+              attributesToRetrieve: [
+                "id",
+                "title",
+                "description",
+                "handle",
+                "thumbnail",
+                "variants",
+                "variant_sku",
+                "options",
+                "collection_title",
+                "collection_handle",
+                "images",
+              ],
+            },
+            transformer: (product) => ({
+              id: product.id,
+              // other attributes...
+            }),
+          },
+        },
+      },
+    },
+  },
 ];
 
 const modules = {
