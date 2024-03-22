2b7e5fedf2ec7bce622e845eb4f65eb712cd5274
shaoxuan <shaoxuanhinhua@gmail.com>
2024-03-22 08:11:10 +0800

    Fix bug

--- src/subscribers/order-placed.ts
+++ src/subscribers/order-placed.ts
@@ -60,25 +60,6 @@ export default async function handleOrderPlaced({
     }
     console.log("Message sent: %s", info.messageId);
   });
-
-  // ------------- Sendgrid ------------------
-
-  // Use sendgrid when client have their own domain email
-  // try {
-  //   await sendGridService.sendEmail({
-  //     templateId: "d-6a5267b599a34291b06288209bdaf1c0",
-  //     from: "shaoxuandev10@gmail.com",
-  //     to: order.email,
-  //     dynamic_template_data: {
-  //       // any data necessary for your template...
-  //       items: order.items,
-  //       status: order.status,
-  //     },
-  //   });
-  //   console.log("email sent!");
-  // } catch (error) {
-  //   console.log("email not sent :(", error);
-  // }
 }
 
 export const config: SubscriberConfig = {
